732c8a3f269907ce6091827ec906ff29014ce1c6
mathiasmenotarg <maelysbarreteau23@gmail.com>
2025-09-27 11:12:09 +0200

    create site and add two reviews

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+
+hamburger.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
